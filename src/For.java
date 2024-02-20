6871ab959b8d29a19b571d5712d3556681549fce
douglas4735 <douglas4735@gmail.com>
2024-02-20 19:29:32 -0400

    alterações

--- src/For.java
+++ src/For.java
@@ -1,14 +1,16 @@
 public class For {
     public static void main(String[] args) {
 
-        String alunos[] = {"douglas","fernanda","ester"};
-
-        for(String aluno : alunos){
-            System.out.println("Nome do aluno é : " + aluno);
-        }
-        /*for (int x = 0; x < alunos.length; x++){
+        String alunos [] = {"douglas","fernanda","ester"};
+        for (int x = 0; x < alunos.length; x++){
             
             System.out.println("O aluno no indice x = " + x + " é " + alunos[x]);
-        }*/
+        }
     }
 }
+
+         /* 
+        for(String aluno : alunos){
+            System.out.println("Nome do aluno é : " + aluno);
+        }*/
+    
