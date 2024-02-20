b8bc3c6edeb64705e37b21d6eb5e98f81cfd58a5
douglas4735 <douglas4735@gmail.com>
2024-02-20 19:56:53 -0400

    alterações03

---
+++ src/Contador.java
@@ -0,0 +1,10 @@
+public class Contador {
+    public static void main(String[] args) {
+        
+        for(int contador = 1; contador <= 100; contador++){
+            if(contador == 21)
+                break;
+                System.out.println(contador);
+            }
+    }
+}
