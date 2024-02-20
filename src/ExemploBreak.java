a01074e1e473ebcd6362b35a1f816893d0771b40
douglas4735 <douglas4735@gmail.com>
2024-02-20 19:43:41 -0400

    alterações02

--- src/ExemploBreak.java
+++ src/ExemploBreak.java
@@ -1,5 +1,13 @@
 public class ExemploBreak {
     public static void main(String[] args) {
+
+        for (int numero = 1; numero <= 5; numero++){
+            if(numero == 3)
+                continue;
+            
+            System.out.println(numero);
+        }
+            System.out.println("Fim");
         
     }
 }
