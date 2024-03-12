aaacc78ba1faf59f5de35a1b41b23b5549c047d2
douglas4735 <douglas4735@gmail.com>
2024-03-12 19:34:13 -0400

    novas alterações

---
+++ src/ContaBancaria.java
@@ -0,0 +1,36 @@
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+
+public class ContaBancaria {
+    public static void main(String[] args) {
+       try{
+       Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+       System.out.println("Digite seu nome: ");
+       String nome = sc.next();
+
+       System.out.println("Digite o numero da sua agencia: ");
+       int agencia = sc.nextInt();
+
+
+       System.out.println("Digite o numero da sua conta: ");
+       int conta = sc.nextInt();
+        
+       System.out.println("Digite o valor para saque?");
+       double saque = sc.nextDouble();
+
+       System.out.println("Olá, " + nome.toUpperCase());
+       System.out.println("Minha agencia é: " + agencia);
+       System.out.println("O numero da minha conta é: " + conta);
+       System.out.println("E o valor informado para saque é: " + saque);
+
+       sc.close();
+        }
+        
+        catch (InputMismatchException e){
+            System.err.println("Os campos idade e altura precisam ser numericos.");
+        }
+    }  
+}
--- src/Treinamanto.java
+++ src/Treinamanto.java
@@ -1,38 +1,9 @@
-import java.util.Scanner;
-import java.util.InputMismatchException;
-import java.util.Locale;
+
 
 public class Treinamanto {
     /*Os codigos escritos aqui são somente para treina sendo que sempre averá mudanças */
     public static void main(String[] args) {
 
-        try{
-       Scanner sc = new Scanner(System.in).useLocale(Locale.US);
-
-       System.out.println("Digite seu nome: ");
-       String nome = sc.next();
-
-       System.out.println("Digite seu sobreNome: ");
-       String sobreNome = sc.next();
-
-
-       System.out.println("Digite sua idade: ");
-       int idade = sc.nextInt();
-        
-       System.out.println("Digite sua altura");
-       double altura = sc.nextDouble();
-
-       System.out.println("Olá,me chamo " + nome.toUpperCase() + " " + sobreNome );
-       System.out.println( " Tenho " + idade + " anos.");
-       System.out.println("Minha altura é " + altura + "cm.");
-
-       sc.close();
-        }
-        
-        catch (InputMismatchException e){
-            System.err.println("Os campos idade e altura precisam ser numericos.");
-        }
-        
-}
+    }
         
 }
