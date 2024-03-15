795dd0d706e5146bddbcd98b872bd4a84481ab76
douglas4735 <douglas4735@gmail.com>
2024-03-14 21:51:42 -0400

    disciplina alterado para AlunoDisciplina

---
+++ src/AlunoDisciplina.java
@@ -0,0 +1,18 @@
+public class AlunoDisciplina {
+    double nota;
+    String disciplina;
+
+
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+    public double getNota() {
+        return nota;
+    }
+    public void setDisciplina(String disciplina){
+        this.disciplina = disciplina;
+    }
+    public String getDisciplina(){
+        return disciplina;
+    }
+}
